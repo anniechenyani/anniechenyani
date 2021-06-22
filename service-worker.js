1b0efb430900b9d267398e20044937b32bac8db3
Anniesiningxuan <1924015132@qq.com>
2021-06-22 22:46:48 +0800

    fix: version1.0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0be190f3b2ac8328b9ed811e118759ac.js"
+  "https://anniechenyani.github.io/anniechenyani/precache-manifest.2d1ca0480bf61d3ffdb329aa558998d2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "person-page"});
